9a2f09f6f635039375ea87a3275cf17af8ef849d
Rlvenancio2021 <91288827+Rlvenancio2021@users.noreply.github.com>
2021-09-25 14:43:20 -0300

    Slice
    Função utilizada para realizar cortes no texto no caso [4;-1] assim será exibido a partir do 5° caractere até o último.

--- Capitulo5_Manipulacao_e_JSON/3_3_Manipula_Arquivos/inventario.py
+++ Capitulo5_Manipulacao_e_JSON/3_3_Manipula_Arquivos/inventario.py
@@ -9,5 +9,7 @@ while opcao>0 and opcao<4:
     elif opcao==2:
         persistir(inventario)
     elif opcao==3:
-        print(exibir())
+        resultado=exibir()
+        for linha in resultado:
+            print(linha[4:-1])
     opcao=chamarMenu()
