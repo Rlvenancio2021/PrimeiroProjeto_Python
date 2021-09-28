381573fc4022d10612759864bee6d7ec52ddc3db
Rlvenancio2021 <91288827+Rlvenancio2021@users.noreply.github.com>
2021-09-27 22:11:04 -0300

    incluso forma de quebra de texto

--- Capitulo5_Manipulacao_e_JSON/3_3_Manipula_Arquivos/inventario.py
+++ Capitulo5_Manipulacao_e_JSON/3_3_Manipula_Arquivos/inventario.py
@@ -13,4 +13,9 @@ while opcao>0 and opcao<4:
         for linha in resultado:
             print(linha[linha.find(";")+1:-1]) # Encontra o primero ";" e apresenta o conteúdo a partir dele,
             #considerando iniciando na posição zero.
+
+            lista=linha.split(";") # quebra o arquivo a cada ";" encontrado.
+            print("Data.........:", lista[1]) # consideradndo que patrimonio esta na posição zero.
+            print("Descrição....:", lista[2])
+            print("Departamento.:", lista[3])
     opcao=chamarMenu()
